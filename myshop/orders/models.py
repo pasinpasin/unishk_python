ea9df351e4246e0320e779ae2ca8ff50a4b58795
unknown <gentiangj@gmail.com>
2023-02-21 14:26:50 +0100

    210223

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -3,6 +3,12 @@ from django.db import models
 # Create your models here.
 from django.db import models
 from shop.models import Product
+from decimal import Decimal
+from django.core.validators import MinValueValidator, \
+                                   MaxValueValidator
+from coupons.models import Coupon
+
+
 class Order(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -13,6 +19,14 @@ class Order(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
+    coupon = models.ForeignKey(Coupon,
+                               related_name='orders',
+                               null=True,
+                               blank=True,
+                               on_delete=models.SET_NULL)
+    discount = models.IntegerField(default=0,
+                                   validators=[MinValueValidator(0),
+                                       MaxValueValidator(100)])
     class Meta:
         ordering = ['-created']
         indexes = [
@@ -21,7 +35,17 @@ class Order(models.Model):
     def __str__(self):
         return f'Order {self.id}'
     def get_total_cost(self):
+         total_cost = self.get_total_cost_before_discount()
+         return total_cost - self.get_discount()
+    def get_total_cost_before_discount(self):
         return sum(item.get_cost() for item in self.items.all())
+    def get_discount(self):
+        total_cost = self.get_total_cost_before_discount()
+        if self.discount:
+            return total_cost * (self.discount / Decimal(100))
+        return Decimal(0)
+    
+
 class OrderItem(models.Model):
     order = models.ForeignKey(Order,
                               related_name='items',
@@ -36,3 +60,4 @@ class OrderItem(models.Model):
            return str(self.id)
     def get_cost(self):
         return self.price * self.quantity
+    
